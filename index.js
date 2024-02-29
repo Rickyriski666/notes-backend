77be08088283b194318b39add9bbf277a41b7d00
Ricky Riskiawan <rickyrizkiawan666@gmail.com>
2024-03-01 06:10:45 +0700

    add: middleware errorhandler, requestlogger, unknownendpoint

--- index.js
+++ index.js
@@ -2,22 +2,17 @@ const express = require('express');
 const Note = require('./src/models/note');
 const app = express();
 const logger = require('./src/utils/logger');
+const middleware = require('./src/middleware');
+const requestLogger = require('./src/middleware/requestLogger');
+const unknownEndpoint = require('./src/middleware/unknownEndpoint');
 const notesRouter = require('./src/controllers/notes');
 require('dotenv').config();
 
 const cors = require('cors');
 app.use(cors());
 
-const requestLogger = (request, response, next) => {
-  logger.info('Method:', request.method);
-  logger.info('Path:  ', request.path);
-  logger.info('Body:  ', request.body);
-  logger.info('---');
-  next();
-};
-
 app.use(express.json());
-app.use(requestLogger);
+app.use(middleware.requestLogger);
 
 app.get('/', (req, res) => {
   res.send(`<h1>Hello World!</h1>`);
@@ -25,31 +20,9 @@ app.get('/', (req, res) => {
 
 app.use('/api/notes', notesRouter);
 
-const unknownEndpoint = (req, res) => {
-  res.status(404).send({ error: 'unknown endpoint' });
-};
+app.use(middleware.unknownEndpoint);
 
-app.use(unknownEndpoint);
-
-const errorHandler = (error, req, res, next) => {
-  logger.info(error.message);
-
-  if (error.name === 'CastError') {
-    return res.status(400).json({
-      status: 'error',
-      message: 'malformatted id'
-    });
-  }
-
-  if (error.name === 'ValidationError') {
-    return res.status(400).json({
-      status: ' Failed to save',
-      message: error.message
-    });
-  }
-};
-
-app.use(errorHandler);
+app.use(middleware.errorHandler);
 
 const PORT = process.env.PORT || 3001;
 app.listen(PORT, () => {
